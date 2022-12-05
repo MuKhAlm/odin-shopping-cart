e09cc1dde642069fd4f21eec0464197a1235b1a1
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-12-05 20:56:48 +0000

    Add empty and styles main
    Reason:
    * Main component is going to contain most of the websites functionality inside of it.
    
    Effect:
    * Create, style Main component and add it to App.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import './App.css'
+import Main from './components/Main/Main'
 
 function App () {
   return (
     <div id='App'>
       <Header></Header>
+      <Main></Main>
       <Footer></Footer>
     </div>
   )
