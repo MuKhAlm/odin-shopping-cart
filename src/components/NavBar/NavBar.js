66eb433874c7d1a258eead328b4cfe95b6c1273b
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-12-06 20:13:34 +0000

    Dev/Fix: Add a link to cart page in nav bar
    Effects:
    * Rename NavBar component function to the correct name.
    * Remove useless styling from NavBar.

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar () {
   return (
     <div id='nav-bar'>
       <Link to={'/'}>Shopping</Link>
-      <span>Cart</span>
+      <Link to={'/cart'}>Cart</Link>
     </div>
   )
 }
