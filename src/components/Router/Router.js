840d3d1d4e4fa1618c1cb79706ada6e30eedcd5c
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-12-06 18:56:34 +0000

    Dev: Set up a router component for the website
    Reason:
    * To navigate between shopping page and cart page.

---
+++ src/components/Router/Router.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from '../../App'
+
+export default function Router () {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} ></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
+import Router from './components/Router/Router'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 )
