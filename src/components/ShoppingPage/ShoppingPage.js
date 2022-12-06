9d197d3bdb5cca041aa611c30c379ce5bc204237
MuKhAlt <muslim.khaldoon.alt@gmail.com>
2022-12-06 18:24:53 +0000

    Dev: Populate shopping page with shopping items
    Effects:
    * Add multiple ShoppingItem components to ShoppingPage.
    * Add items images to assets for them to be imported.

--- src/components/ShoppingPage/ShoppingPage.js
+++ src/components/ShoppingPage/ShoppingPage.js
@@ -3,13 +3,26 @@ import ShoppingItem from '../ShoppingItem/ShoppingItem'
 
 import './ShoppingPage.css'
 
+import triangle from '../../assets/triangle.svg'
+import eightPointStar from '../../assets/eightPointStar.svg'
+import circle from '../../assets/circle.svg'
+import octagon from '../../assets/octagon.svg'
+import rectangle from '../../assets/rectangle.svg'
+import rhombus from '../../assets/rhombus.svg'
+import square from '../../assets/square.svg'
+import star from '../../assets/star.svg'
+
 export default function ShoppingPage () {
   return (
     <div id='shopping-page'>
-      <ShoppingItem></ShoppingItem>
-      <ShoppingItem></ShoppingItem>
-      <ShoppingItem></ShoppingItem>
-      <ShoppingItem></ShoppingItem>
+      <ShoppingItem itemID={0} image={triangle} name='Triangle' price={300} onClick={() => {}} ></ShoppingItem>
+      <ShoppingItem itemID={1} image={star} name='Star' price={1000} onClick={() => {}} ></ShoppingItem>
+      <ShoppingItem itemID={2} image={eightPointStar} name='Eight Point Star' price={1600} onClick={() => {}} ></ShoppingItem>
+      <ShoppingItem itemID={3} image={rhombus} name='Rhombus' price={400} onClick={() => {}} ></ShoppingItem>
+      <ShoppingItem itemID={4} image={square} name='square' price={400} onClick={() => {}} ></ShoppingItem>
+      <ShoppingItem itemID={5} image={rectangle} name='Rectangle' price={400} onClick={() => {}} ></ShoppingItem>
+      <ShoppingItem itemID={6} image={octagon} name='Octagon' price={800} onClick={() => {}} ></ShoppingItem>
+      <ShoppingItem itemID={7} image={circle} name='Circle' price={0} onClick={() => {}} ></ShoppingItem>
     </div>
   )
 }
